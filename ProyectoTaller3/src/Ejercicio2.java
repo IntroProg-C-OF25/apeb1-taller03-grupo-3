ce636aad1324ec0ceb80806745fa5354c78ff490
Chente15 <vichito2006ruiz@gmail.com>
2024-10-24 12:56:47 -0500

    Ejercicio2

--- ProyectoTaller3/src/Ejercicio2.java
+++ ProyectoTaller3/src/Ejercicio2.java
@@ -1,12 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Usuario iTC
- */
 public class Ejercicio2 {
+    public static void main(String[] args) {
+        int a= 10; 
+        int b= 2;
+        double respuesta;
+        respuesta = Math.pow(a, b)+10;
+        System.out.println("respuesta = " + respuesta);
+        
+    }
     
 }
--- ProyectoTaller3/src/Ejercicio3.java
+++ ProyectoTaller3/src/Ejercicio3.java
@@ -1,12 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author Usuario iTC
- */
 public class Ejercicio3 {
-    
+    public static void main(String[] args) {
+        int a= 10;
+        int b= 2;
+        double respuesta;
+        respuesta = Math.pow(a, b)+ Math.sqrt(16)+((20)/12);
+        System.out.println("respuesta = " + respuesta);
+    }
 }
